d39bc3f490e0236dd29707ad74c9a0749e82749b
CarlosEduardoPM <linkedincarlos321@gmail.com>
2026-03-21 19:58:22 -0300

    Revert "feat: add model of reports and users"
    This reverts commit 8c2196c039ef1b2a5ec0b09fb2109275481cd1bb.

--- backend/app/models/user_model.py
+++ backend/app/models/user_model.py
@@ -1,17 +1,10 @@
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column, Integer, String
 from ..database import Base
 
 
 class User(Base):
     __tablename__ = "users"
 
-    id = Column(Integer, primary_key=True)
-    cpf = Column(String(11), unique=True, nullable=False, index=True)
-    name = Column(String(100), index=True)
-    email = Column(String(100), unique=True, nullable=False)
-    role = Column(String(100), nullable=False) #Employee, analyst e manager
-    number = Column(String(20), index=True, nullable=True)
-    status_usuario = Column(Boolean, nullable=False, default=False) # False = off/True = On
-
-    
-
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(100))
+    email = Column(String(100), unique=True, index=True)
