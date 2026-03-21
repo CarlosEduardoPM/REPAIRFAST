c67bfc835c73c8acc5d4a065bd393c9e58d2c62e
CarlosEduardoPM <linkedincarlos321@gmail.com>
2026-03-21 20:07:55 -0300

    feat: add model user_model

--- backend/app/models/user_model.py
+++ backend/app/models/user_model.py
@@ -1,10 +1,16 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Boolean
 from ..database import Base
 
 
 class User(Base):
     __tablename__ = "users"
 
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String(100))
-    email = Column(String(100), unique=True, index=True)
+    id = Column(Integer, primary_key=True)
+    cpf = Column(String(11), unique=True, nullable=False, index=True)
+    name = Column(String(100), index=True)
+    email = Column(String(100), unique=True, nullable=False)
+    role = Column(String(50), nullable=False) #Employee, analyst e manager
+    number = Column(String(20), index=True, nullable=True)
+    status_usuario = Column(Boolean, nullable=False, default=False) # False = off/True = On
+
+    
