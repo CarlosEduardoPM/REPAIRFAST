5f9336ed98a634601223b60676ebf03c41fef500
CarlosEduardoPM <linkedincarlos321@gmail.com>
2026-03-24 16:51:46 -0300

    feat: add Alembic migrations

--- backend/app/models/reports_model.py
+++ backend/app/models/reports_model.py
@@ -1,6 +1,6 @@
 from sqlalchemy import Text,Enum,DateTime, Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
-from ..database import Base
+from app.database import Base
 from datetime import datetime
 
 class Report(Base):
