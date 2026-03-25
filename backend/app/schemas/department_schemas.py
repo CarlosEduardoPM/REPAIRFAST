3701516ce977b6b41b527b0e38d3900d630fb69f
CarlosEduardoPM <linkedincarlos321@gmail.com>
2026-03-25 15:23:13 -0300

    feat: add crud endpoint department

---
+++ backend/app/schemas/department_schemas.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel, EmailStr
+
+class DepartmentBase(BaseModel):
+    name: str
+
+class DepartmentCreate(DepartmentBase):
+    pass
+
+class DepartmentResponse(DepartmentBase):
+    id: int
+
+    class Config:
+        from_attributes = True
