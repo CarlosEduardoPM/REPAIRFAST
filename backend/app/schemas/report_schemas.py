0038220b84343d8b5981b4560cfd0160cd4f9334
CarlosEduardoPM <linkedincarlos321@gmail.com>
2026-04-06 16:06:42 -0300

    feat:add analyst role in route report, add status enum and created_at in report schema

--- backend/app/schemas/report_schemas.py
+++ backend/app/schemas/report_schemas.py
@@ -1,11 +1,16 @@
 from pydantic import BaseModel, Field
 from enum import Enum
 from typing import Optional
+from datetime import datetime
 
 class PriorityEnum(str, Enum):
     low = "low"
     medium = "medium"
     high = "high"
+class StatusEnum(str, Enum):
+    open        = "open"
+    in_progress = "in_progress"
+    closed      = "closed"
 
 class UserCreateReport(BaseModel):
     title: Optional[str] = None
@@ -13,14 +18,19 @@ class UserCreateReport(BaseModel):
     attachment: Optional[str] = None
     category: Optional[str] = None
     priority: Optional[PriorityEnum] = None
+    
+
 class UserUpdateReport(BaseModel):
     title: str 
     description: str
     attachment: Optional[str] = Field(default=None, description="File URL or path")
     category: str
     priority: PriorityEnum = PriorityEnum.medium 
+    status:      StatusEnum        
+    created_at: Optional[datetime] = None
     
 
+     
 class ReportResponse(BaseModel):
     id: int
     title: str
@@ -28,6 +38,9 @@ class ReportResponse(BaseModel):
     attachment: str | None = None
     category: str
     priority: PriorityEnum
+
     class Config:
-        from_attributes = True 
+        from_attributes = True
+
+
 
